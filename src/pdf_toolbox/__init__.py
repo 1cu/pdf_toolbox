04035c2098cb411354ffc3c5bf15011dd48eb9f5
Jens <1742418+1cu@users.noreply.github.com>
2025-09-11 09:57:40 +0200

    Use absolute imports across CLI modules

--- src/pdf_toolbox/__init__.py
+++ src/pdf_toolbox/__init__.py
@@ -1,5 +1,10 @@
 """PDF toolbox package providing various PDF utilities."""
 
-from .utils import ensure_libs, sane_output_dir, update_metadata, parse_page_spec
+from pdf_toolbox.utils import (
+    ensure_libs,
+    sane_output_dir,
+    update_metadata,
+    parse_page_spec,
+)
 
 __all__ = ["ensure_libs", "sane_output_dir", "update_metadata", "parse_page_spec"]
