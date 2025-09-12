d62cb2fec1151713e717ecb4c8adc66effb68782
Jens <1742418+1cu@users.noreply.github.com>
2025-09-13 00:51:07 +0200

    Centralize logging configuration with GUI control

--- src/pdf_toolbox/__init__.py
+++ src/pdf_toolbox/__init__.py
@@ -5,10 +5,19 @@ import sys
 sys.set_int_max_str_digits(0)
 
 from pdf_toolbox.utils import (
+    configure_logging,
     ensure_libs,
+    logger,
     parse_page_spec,
     sane_output_dir,
     update_metadata,
 )
 
-__all__ = ["ensure_libs", "parse_page_spec", "sane_output_dir", "update_metadata"]
+__all__ = [
+    "configure_logging",
+    "ensure_libs",
+    "logger",
+    "parse_page_spec",
+    "sane_output_dir",
+    "update_metadata",
+]
